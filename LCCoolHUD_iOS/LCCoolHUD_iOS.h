66b3c653e51131802d22c6686897c7a13104023a
stubbornnessness <1219721814yxj>
2016-10-12 01:19:45 +0800

    1.1版本开始支持carthage

---
+++ LCCoolHUD_iOS/LCCoolHUD_iOS.h
@@ -0,0 +1,19 @@
+//
+//  LCCoolHUD_iOS.h
+//  LCCoolHUD_iOS
+//
+//  Created by yuanxiaojun on 2016/10/12.
+//  Copyright © 2016年 袁晓钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LCCoolHUD_iOS.
+FOUNDATION_EXPORT double LCCoolHUD_iOSVersionNumber;
+
+//! Project version string for LCCoolHUD_iOS.
+FOUNDATION_EXPORT const unsigned char LCCoolHUD_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LCCoolHUD_iOS/PublicHeader.h>
+
+
--- Podfile
+++
@@ -1,3 +0,0 @@
-pod 'JGProgressHUD'
-
-pod 'LCLoadingHUD'
